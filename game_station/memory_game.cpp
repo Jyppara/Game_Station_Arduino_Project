fba4dd375655d3810ab30f39122d1bae92615e15
Jyppara <jyri.maatta@tuni.fi>
2023-10-01 12:41:40 +0300

    Cards are displayed and can be choosed

--- game_station/memory_game.cpp
+++ game_station/memory_game.cpp
@@ -2,8 +2,6 @@
 #include "memory_game.h"
 #include <LiquidCrystal.h>
 
-int potentioMeterIndex = 0;
-
 void memoryGameplay()
 {
     // This function is used to the memory gameplay.
@@ -16,7 +14,7 @@ void memoryGameplay()
     lcd.setCursor(0, 0);
     lcd.print("Memorize the");
     lcd.setCursor(0, 1);
-    lcd.print("sequence!");
+    lcd.print("cards!");
 
     while (digitalRead(2) == LOW)
     {
@@ -27,25 +25,87 @@ void memoryGameplay()
         // Wait for the player to release the button.
     }
     lcd.clear();
+    startAnimation(8, mapPotentiometerValue(8));
     while (digitalRead(2) == LOW)
     {
-        lcd.clear();
-        int potentiometerValue = analogRead(A0);
-        potentioMeterIndex = map(potentiometerValue, 0, 1023, 0, 31);
-        Serial.println(potentioMeterIndex);
-        if (potentioMeterIndex < 16)
-        {
-            printSpecialChar(0, potentioMeterIndex, cardChar, 6);
-        }
-        else
-        {
-            printSpecialChar(1, potentioMeterIndex - 16, cardChar, 6);
-        }
-        
+        printAllCards(8, mapPotentiometerValue(8));
         delay(50);
     }
     while (digitalRead(2) == HIGH)
     {
         // Wait for the player to release the button.
+    }
+}
+
+int mapPotentiometerValue(int numberOfCards)
+{
+    // This function is used to read the potentiometer value and print the corresponding card on the screen.
+    int potentiometerValue = analogRead(A0);
+    int potentioMeterIndex = map(potentiometerValue, 0, 1023, 0, numberOfCards - 1);
+    Serial.println(potentioMeterIndex); // for debugging
+    return potentioMeterIndex;
+}
+
+void printAllCards(int numberOfCards, int potentioMeterIndex)
+{
+    // This function is used to print all the cards on the screen.
+    int indent = (16 - (numberOfCards / 2)) / 2;
+    for (int i = 0; i < numberOfCards; i++)
+    {
+        if (i < (numberOfCards / 2))
+        {
+            if(i == potentioMeterIndex)
+            {
+                printSpecialChar(0, indent + i, selectedCardChar, 1);
+            }
+            else
+            {
+                printSpecialChar(0, indent + i, cardChar, 0);
+            }
+        }
+        else
+        {
+            if(i == potentioMeterIndex)
+            {
+                printSpecialChar(1, indent + i - (numberOfCards / 2), selectedCardChar, 1);
+            }
+            else
+            {
+                printSpecialChar(1, indent + i - (numberOfCards / 2), cardChar, 0);
+            }
+        }
+    }
+}
+
+void startAnimation(int numberOfCards, int potentioMeterIndex)
+{
+    // This function is used to print all the cards on the screen.
+    int indent = (16 - (numberOfCards / 2)) / 2;
+    for (int i = 0; i < numberOfCards; i++)
+    {
+        if (i < (numberOfCards / 2))
+        {
+            if(i == potentioMeterIndex)
+            {
+                printSpecialChar(0, indent + i, selectedCardChar, 1);
+            }
+            else
+            {
+                printSpecialChar(0, indent + i, cardChar, 0);
+            }
+        }
+        else
+        {
+            if(i == potentioMeterIndex)
+            {
+                printSpecialChar(1, indent + i - (numberOfCards / 2), selectedCardChar, 1);
+            }
+            else
+            {
+                printSpecialChar(1, indent + i - (numberOfCards / 2), cardChar, 0);
+            }
+        }
+
+        delay(200);
     }
 }
--- game_station/memory_game.h
+++ game_station/memory_game.h
@@ -4,15 +4,28 @@
 #include <LiquidCrystal.h>
 
 const byte cardChar[] = {
-    B11111,
-	B11011,
-	B10101,
 	B11111,
-	B11111,
-	B10101,
-	B11011,
+	B10001,
+	B10001,
+	B10001,
+	B10001,
+	B10001,
+	B10001,
 	B11111};
 
+const byte selectedCardChar[] = {
+	B00000,
+	B01110,
+	B01110,
+	B01110,
+	B01110,
+	B01110,
+	B01110,
+	B00000};
+
 void memoryGameplay();
+int mapPotentiometerValue(int numberOfCards);
+void printAllCards(int numberOfCards, int potentioMeterIndex);
+void startAnimation(int numberOfCards, int potentioMeterIndex);
 
 #endif
